2ddcb18756ce60449b347c00e3e58b0835f55c9a
ritchielrez@gmail.com <ritchielrez@gmail.com>
2021-03-28 16:12:43 +0600

    Fixing the options array again

--- browserinstall.sh
+++ browserinstall.sh
@@ -9,11 +9,11 @@
 # An array of browser names. You can edit htis list to add/remove
 # search engines. The format must be: [browser_executable_name]="Browser Name".
 declare -a options=(
-"brave-browser : Brave"
-"chromium-browser : Chromium"
-"firefox : Firefox"
-"google-chrome : Google Chrome"
-"vivaldi : Vivaldi"
+"brave-browser"
+"chromium-browser"
+"firefox"
+"google-chrome"
+"vivaldi"
 )
 
 choice=$(printf '%s\n' "${options[@]}" | rofi -dmenu -p "Pick up a browser to install")
