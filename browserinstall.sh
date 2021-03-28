143b0a48bee7fe91f49a98d5f6bc5f8bcce6bd8c
ritchielrez@gmail.com <ritchielrez@gmail.com>
2021-03-28 14:24:51 +0600

    Added a option array in browserinstall script

--- browserinstall.sh
+++ browserinstall.sh
@@ -6,4 +6,13 @@
 # Contributors: RitchielRez
 #
 
+# An array of browser names. You can edit htis list to add/remove
+# search engines. The format must be: [browser_executable_name]="Browser Name".
+declare -a options
+options[brave-browser]="Brave"
+options[chromium-browser]="Chromium"
+options[firefox]="Firefox"
+options[google-chrome]="Google Chrome"
+options[vivaldi]="Vivaldi"
+
 
