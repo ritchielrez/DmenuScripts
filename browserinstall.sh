04618351a3af2d5e6799913c86f716cd27c12f7e
RitchielRez <ritchielrez@gmail.com>
2021-03-28 13:20:33 +0600

    Created a browserinstall script

---
+++ browserinstall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Script name: browserinstall
+# Description: Install various browsers in Ubuntu Linux
+# Dependencies: rofi or dmenu and Ubuntu
+# Contributors: RitchielRez
+#
+
+
