a8bde1cb091aedbe7d20476012793d9498523d62
RitchielRez <ritchielrez@gmail.com>
2021-10-05 11:51:57 +0600

    A new script to enable natural scrolling

--- inverse-scroll.sh
+++ inverse-scroll.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Get id of touchpad and the id of the field corresponding to
-# tapping to click
+# natural scrolling
 id=`xinput list | grep "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
-tap_to_click_id=`xinput list-props $id | \
-                      grep "Tapping Enabled (" \
+natural_scrolling_id=`xinput list-props $id | \
+                      grep "Natural Scrolling Enabled (" \
                       | cut -d'(' -f2 | cut -d')' -f1`
 
 # Set the property to true
-xinput --set-prop $id $tap_to_click_id 1
+xinput --set-prop $id $natural_scrolling_id 1
